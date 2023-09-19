e4bf1aad6bcf8f837b55ad37fd5cf5c171ebfa92
KellyMadland <kellymadland@gmail.com>
2023-09-19 15:04:54 -0500

    Assignment Operator Demo

---
+++ SecondProject/src/variables/AssignmentOperatorDemo.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class AssignmentOperatorDemo {
+	public static void main(String args[]) {
+	       
+	   int j, k;
+	   j = 10; 
+	   j = 5; 
+	   k = j; 
+	   System.out.println("j is : " + j);
+	   System.out.println("k is : " + k);
+
+	       
+	   k = j = 10; 
+	   System.out.println("j is : " + j);
+	   System.out.println("k is : " + k);
+	   }
+	}
