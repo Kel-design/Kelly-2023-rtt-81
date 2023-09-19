cfdd438f0bc1d0c7240e153f5eb71aa4c14846bb
KellyMadland <kellymadland@gmail.com>
2023-09-19 13:09:30 -0500

    Cleaning up my workspace

---
+++ SecondProject/src/variables/EscapeCharacters.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class EscapeCharacters {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is a tab character ->\t<-");
+		
+		System.out.println("This is line 1\nThis is line 2");
+		
+		System.out.println("To get a slash to print we need 2 of them \\");
+		
+		System.out.println("1\tOne");
+		System.out.println("10\tTen");
+		System.out.println("100\tOne Hundred");
+		
+		System.out.println("1\tOne\t\tCol3");
+		System.out.println("10\tTen\t\tCol3");
+		System.out.println("100\tOne Hundred\tCol3");
+		
+		System.out.println("This is a quote\" in a string");
+		
+	}
+
+}
