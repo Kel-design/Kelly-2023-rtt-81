5ea9d4b10cda2312d86d533e2731db6e3b85a1d9
KellyMadland <kellymadland@gmail.com>
2023-09-19 20:35:29 -0500

    Start of PA Operators and Numbers

---
+++ CoreJavaBasics/src/com/perscholas/java_basics/PracticeAssignmentOperatorsAndNumbers.java
@@ -0,0 +1,24 @@
+package com.perscholas.java_basics;
+
+public class PracticeAssignmentOperatorsAndNumbers {
+
+	public static void main(String[] args) {
+		
+		// Task 1
+		
+		
+		int x = 2;
+		x <<= 1;
+		System.out.println(Integer.toBinaryString(x));
+		System.out.printf("%.2f, value");
+		
+		// NEED HELP PRINTING FROM INTEGER TO TWO DECIMALS
+		
+		
+		
+		
+	
+			
+		
+	}
+}
