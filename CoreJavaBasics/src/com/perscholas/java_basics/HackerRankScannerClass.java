095df4212b6d18b3c35d3932fc03ba481229d6ad
KellyMadland <kellymadland@gmail.com>
2023-09-20 09:48:43 -0500

    First HackerRank Challenge (Scanner Class)

---
+++ CoreJavaBasics/src/com/perscholas/java_basics/HackerRankScannerClass.java
@@ -0,0 +1,31 @@
+package com.perscholas.java_basics;
+
+// This is code that worked correctly in HackerRank in Scanner Class Challenge
+import java.util.*;
+
+public class HackerRankScannerClass {
+	
+	
+
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	        a = 42;
+	    
+	        System.out.println(a);
+	    
+	        int b = scan.nextInt();
+	        b = 100;
+	    
+	        System.out.println(b);
+	        
+	        int c = scan.nextInt();
+	        c = 125;
+	    
+	        System.out.println(c);
+	        scan.close();
+	            
+	}
+	}
+
+
