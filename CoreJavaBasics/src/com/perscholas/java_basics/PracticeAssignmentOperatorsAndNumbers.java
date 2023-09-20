f7324249e593953baeccc0333f7b4d19a66572a5
KellyMadland <kellymadland@gmail.com>
2023-09-20 08:25:38 -0500

    Cont working

--- CoreJavaBasics/src/com/perscholas/java_basics/PracticeAssignmentOperatorsAndNumbers.java
+++ CoreJavaBasics/src/com/perscholas/java_basics/PracticeAssignmentOperatorsAndNumbers.java
@@ -6,11 +6,11 @@ public class PracticeAssignmentOperatorsAndNumbers {
 		
 		// Task 1
 		
-		
+
 		int x = 2;
 		x <<= 1;
 		System.out.println(Integer.toBinaryString(x));
-		System.out.printf("%.2f, value");
+		System.out.printf("%.2f", value);
 		
 		// NEED HELP PRINTING FROM INTEGER TO TWO DECIMALS
 		
