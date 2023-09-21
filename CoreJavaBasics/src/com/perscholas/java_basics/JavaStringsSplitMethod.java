d0f7e4f668acaa94eb0e7c0b2a469ff325f13e21
KellyMadland <kellymadland@gmail.com>
2023-09-21 18:10:33 -0500

    Strings Split Method

---
+++ CoreJavaBasics/src/com/perscholas/java_basics/JavaStringsSplitMethod.java
@@ -0,0 +1,16 @@
+package com.perscholas.java_basics;
+
+import java.util.Arrays;
+
+public class JavaStringsSplitMethod {
+
+	public static void main(String[] args) {
+		String vowels = "a::b::c::d:e";
+		
+		String[] result = vowels.split("::");
+		
+		System.out.println("result = " + Arrays.toString(result));
+		
+	}
+
+}
