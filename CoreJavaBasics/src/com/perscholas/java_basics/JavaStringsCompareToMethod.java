7870738c2d05cf913ef397bedf184bd9686810f9
KellyMadland <kellymadland@gmail.com>
2023-09-21 18:21:48 -0500

    Strings compareTo() method

---
+++ CoreJavaBasics/src/com/perscholas/java_basics/JavaStringsCompareToMethod.java
@@ -0,0 +1,16 @@
+package com.perscholas.java_basics;
+
+public class JavaStringsCompareToMethod {
+
+	public static void main(String[] args) {
+		String s1 = "hello";
+		String s2 = "hello";
+		String s3 = "hemlo";
+		String s4 = "flag";
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.compareTo(s3));
+		System.out.println(s1.compareTo(s4));
+
+	}
+
+}
