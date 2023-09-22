f43c0faed5f464e4b904d4f7e7ea028cdcb0ba8a
KellyMadland <kellymadland@gmail.com>
2023-09-22 18:13:37 -0500

    still trying

--- CoreJavaBasics/src/controlflow/PAControlFlow7.java
+++ CoreJavaBasics/src/controlflow/PAControlFlow7.java
@@ -7,21 +7,24 @@ public class PAControlFlow7 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Which is your filing status? \n(Single, Married Filing Jointly, Married Filing Separately or Head of Household):  ");
+		System.out.print("What is your filing status?  "
+				+ "\n(Single, Married Filing Jointly, Married Filing Separately or Head of Household)  ");
 		String status = sc.toString();
+		
+		DecimalFormat df = new DecimalFormat("$###,###.00");
+		System.out.print("\nPlease enter your income: ");
 	}
 		//need to give the 4 status choices
 		
 		
-		if Single
-		DecimalFormat df = new DecimalFormat("$###,###.00");
-		System.out.print("Please enter your income: ");
+	
+		
 		
 		
 			
-	}
+	
 		
 
 	}
 
-}
+
