b088b614e407b2033427a39a70c13a3881328136
KellyMadland <kellymadland@gmail.com>
2023-09-25 07:36:20 -0500

    HW and GL for loops

---
+++ CoreJavaBasics/src/guidedlabforloops/DisplaySumOfNNaturalNumbers.java
@@ -0,0 +1,18 @@
+package guidedlabforloops;
+
+public class DisplaySumOfNNaturalNumbers {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int n = 1000;
+		
+		for (int i = 1; i <= n; ++i) {
+			
+			sum += i;
+		}
+		
+		System.out.println("Sum =" + sum);
+
+	}
+
+}
---
+++ CoreJavaBasics/src/guidedlabforloops/PalindromeExample.java
@@ -0,0 +1,10 @@
+package guidedlabforloops;
+
+public class PalindromeExample {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
