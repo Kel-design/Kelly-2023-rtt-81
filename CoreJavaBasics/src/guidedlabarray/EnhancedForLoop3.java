a720574c616e8d1602998173d61a9974caee9a55
KellyMadland <kellymadland@gmail.com>
2023-09-25 20:23:12 -0500

    Guided Lab Array

---
+++ CoreJavaBasics/src/guidedlabarray/EnhancedForLoop3.java
@@ -0,0 +1,12 @@
+package guidedlabarray;
+
+public class EnhancedForLoop3 {
+
+	public static void main(String[] args) {
+		String[] names = { "New York", "Dallas", "Las Vegas", "Florida" };
+		for (String name : names) {
+			System.out.println(name);
+		}
+	}
+
+}
