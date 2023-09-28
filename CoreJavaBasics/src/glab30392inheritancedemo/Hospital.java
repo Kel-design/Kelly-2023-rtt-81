c09a979757fb84bf49798a73581b7be881902223
KellyMadland <kellymadland@gmail.com>
2023-09-28 17:27:40 -0500

    Guided Lab 30392 Basic Inheritance Example

---
+++ CoreJavaBasics/src/glab30392inheritancedemo/Hospital.java
@@ -0,0 +1,12 @@
+package glab30392inheritancedemo;
+
+public class Hospital {
+
+	public static void main(String[] args) {
+		Surgeon s = new Surgeon();
+		s.Doctor_Details();
+		s.Surgeon_Details();
+
+	}
+
+}
---
+++ CoreJavaBasics/src/glab30392inheritancedemo/Surgeon.java
@@ -0,0 +1,9 @@
+package glab30392inheritancedemo;
+
+public class Surgeon extends Doctor {
+	void Surgeon_Details() {
+		System.out.println("Surgeon Detail...");
+		System.out.println(Department = "Cardio");
+	}
+
+}
