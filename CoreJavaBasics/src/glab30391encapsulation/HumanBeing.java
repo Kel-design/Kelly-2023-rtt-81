ab9bac4de1331b57889f50064a94eb649397896e
KellyMadland <kellymadland@gmail.com>
2023-09-28 14:23:15 -0500

    Guided Lab 30391 Encapsulation

--- CoreJavaBasics/src/glab30391encapsulation/HumanBeing.java
+++ CoreJavaBasics/src/glab30391encapsulation/HumanBeing.java
@@ -1,10 +1,43 @@
 package glab30391encapsulation;
 
+
+
 public class HumanBeing {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private float weight;
+	private float height;
+	private float bmi;
 
+	public HumanBeing(float weight, float height, float bmi) {
+		this.weight = weight;
+		this.height = height;
+		this.bmi = bmi;
 	}
 
+	public HumanBeing() {
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float getBmi() {
+		return bmi;
+	}
+
+	public void setBmi(float bmi) {
+		this.bmi = bmi;
+	}
 }
