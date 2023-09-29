1c6030196c1354a22e2e1856d7f0d3a98238e41d
KellyMadland <kellymadland@gmail.com>
2023-09-28 20:58:26 -0500

    Encapsulation Example

---
+++ CoreJavaBasics/src/encapsulationexample/mytest.java
@@ -0,0 +1,16 @@
+package encapsulationexample;
+
+public class mytest {
+
+	public static void main(String[] args) {
+		Person mike = new Person();
+		mike.setName(" Mike");
+		mike.setAge(22);
+		mike.setCompany("Per Scholas");
+		mike.setJob("Technical Instructor");
+		mike.setHobby("learn about Financial Engineering");
+		mike.talkaboutYourself();
+
+	}
+
+}
