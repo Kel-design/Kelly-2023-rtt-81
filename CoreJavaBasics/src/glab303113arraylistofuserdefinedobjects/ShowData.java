3201d64746ed80a459901eb28ab286befa15c0fc
KellyMadland <kellymadland@gmail.com>
2023-10-03 17:56:10 -0500

    glab 303.11.3 Array List of User Defined Objects

---
+++ CoreJavaBasics/src/glab303113arraylistofuserdefinedobjects/ShowData.java
@@ -0,0 +1,20 @@
+package glab303113arraylistofuserdefinedobjects;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ShowData {
+
+	public static void main(String[] args) {
+		// instantiation to class AddDataToArrayList
+		AddDataToArrayList b = new AddDataToArrayList();
+		ArrayList<Book> mybooklist = b.bookdetails();
+		for (Book showValue : mybooklist) {
+			// ---- invoking getter method for getting Data---------
+			System.out.println(showValue.getNumber() + " " + showValue.getName1() + " " + showValue.getCategory() + " "
+					+ showValue.getAuthor());
+		}
+
+	}
+
+}
