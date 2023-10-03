59b293ec1517a0b35fed06770806d548c078d852
KellyMadland <kellymadland@gmail.com>
2023-10-03 11:07:28 -0500

    glab 303111 beginning

---
+++ CoreJavaBasics/src/glab303111genericmethodandclass/MyRunner.java
@@ -0,0 +1,14 @@
+package glab303111genericmethodandclass;
+
+public class MyRunner {
+
+	public static void main(String[] args) {
+		DemoClass dObj = new DemoClass();
+		dObj.genericsMethod(25); // passing int
+		dObj.genericsMethod("Per Scholas"); // passing String
+		dObj.genericsMethod(2563.5); // passing float
+		dObj.genericsMethod('H');//passing Char
+
+	}
+
+}
---
+++ CoreJavaBasics/src/glab303111genericmethodandclass/MyRunner2.java
@@ -0,0 +1,18 @@
+package glab303111genericmethodandclass;
+
+public class MyRunner2 {
+
+	public static void main(String[] args) {
+		GMultipleDatatype<String, Integer> mobj = new GMultipleDatatype("Per Scholas", 11025);
+
+		System.out.println(mobj.getValueOne());
+		System.out.println(mobj.getValueTwo());
+
+		// initialize generic class
+		// with String and String data
+		GMultipleDatatype<String, String> mobj2 = new GMultipleDatatype("Per Scholas", "Non profit");
+		System.out.println(mobj2.getValueOne());
+		System.out.println(mobj2.getValueTwo());
+	}
+
+}
