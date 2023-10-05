733fc1332acd7eafc9bb41dfb993716489af7ba5
KellyMadland <kellymadland@gmail.com>
2023-10-05 15:12:28 -0500

    Hackerrank Try Catch attempt

---
+++ CoreJavaBasics/src/hackerrankchallenges/hackerranktrycatchexceptions.java
@@ -0,0 +1,19 @@
+package hackerrankchallenges;
+
+public class hackerranktrycatchexceptions {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = hello;
+		try {
+			if (y > 0) {
+				int result = Math.toIntExact(x / y);
+				System.out.println(result);
+			}
+		} catch (TypeNotPresentException e) {
+			
+			// TRYING to catch a data type input mismatch here SOMEHOW
+			System.out.println(e.getMessage());
+		}
+	}
+}
